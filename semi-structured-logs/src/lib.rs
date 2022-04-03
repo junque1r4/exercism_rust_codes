30838a92c491dacd50544df4777a5f88fe2d0cf3
Joao Junqueira <dev.junqueira@gmail.com>
2022-04-02 21:48:42 -0300

    Solved semi-structured. Started Allergies exercise. Added some file to gitignore

--- semi-structured-logs/src/lib.rs
+++ semi-structured-logs/src/lib.rs
@@ -1,24 +1,39 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
 #![allow(unused)]
 
-/// various log levels
 #[derive(Clone, PartialEq, Debug)]
 pub enum LogLevel {
     Info,
     Warning,
     Error,
+    Debug
 }
+
 /// primary function for emitting logs
 pub fn log(level: LogLevel, message: &str) -> String {
-    unimplemented!()
+    match level {
+        LogLevel::Info => info(message),
+        LogLevel::Warning => warn(message),
+        LogLevel::Error => error(message),
+        LogLevel::Debug => debug(message),
+    }
 }
+
 pub fn info(message: &str) -> String {
-    unimplemented!()
+    let message = String::from(format!("[INFO]: {}", message));
+    message
 }
+
 pub fn warn(message: &str) -> String {
-    unimplemented!()
+    let message = String::from(format!("[WARNING]: {}", message));
+    message
 }
+
 pub fn error(message: &str) -> String {
-    unimplemented!()
+    let message = String::from(format!("[ERROR]: {}", message));
+    message
+}
+
+pub fn debug(message: &str) -> String {
+    let message = String::from(format!("[DEBUG]: {}", message));
+    message
 }
