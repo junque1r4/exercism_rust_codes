5202c5319df1b01fae755f170fa68bb0ef061d37
junque1r4 <dev.junqueira@gmail.com>
2022-06-06 22:38:01 -0300

    :tada: New exercise completed -> 8/103

--- short-fibonacci/src/lib.rs
+++ short-fibonacci/src/lib.rs
@@ -1,19 +1,28 @@
+use std::vec;
+
 /// Create an empty vector
 pub fn create_empty() -> Vec<u8> {
-    unimplemented!()
+    create_buffer(0)
 }
 
 /// Create a buffer of `count` zeroes.
 ///
 /// Applications often use buffers when serializing data to send over the network.
 pub fn create_buffer(count: usize) -> Vec<u8> {
-    unimplemented!("create a zeroized buffer of {} bytes", count)
-}
+    if count > 0 { let vector = vec![0; count]; vector } else { Vec::new() }
+    }
 
 /// Create a vector containing the first five elements of the Fibonacci sequence.
 ///
 /// Fibonacci's sequence is the list of numbers where the next number is a sum of the previous two.
 /// Its first five elements are `1, 1, 2, 3, 5`.
 pub fn fibonacci() -> Vec<u8> {
-    unimplemented!()
+    let mut fibonacci_vec: Vec<u8> = Vec::new(); 
+    for number in 0..5 {
+        if number == 0 || number == 1 { fibonacci_vec.push(1)} else {
+        fibonacci_vec.push( fibonacci_vec[number-1] + fibonacci_vec[number-2]);
+        }
+    }
+
+     fibonacci_vec
 }
