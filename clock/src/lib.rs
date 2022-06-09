f6e179cbc371faeb366ff8d7340545eb8072541f
junque1r4 <dev.junqueira@gmail.com>
2022-06-08 22:17:34 -0300

    Clock started

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,15 +1,24 @@
-pub struct Clock;
+use std::fmt;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        Clock { hours, minutes }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
         unimplemented!("Add {} minutes to existing Clock time", minutes);
     }
 }
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+    }
+} 
+
