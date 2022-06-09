b6fd9942ee4479bfb5c46461fb43a6eb77944e88
junque1r4 <dev.junqueira@gmail.com>
2022-06-08 22:48:16 -0300

    At this point 13/40 was passed. I need to implement a way to decrease the hour if it > 24

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::{fmt};
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Clock {
@@ -8,11 +8,43 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
+        let mut new_hours = hours;
+        let mut new_minutes = minutes;
+
+        // match new_hours >= 24 {
+        //     true => todo!(),
+        //     false => todo!(),
+        // }
+
+        // match new_minutes >= 60 {
+        //     true => todo!(),
+        //     false => todo!(),
+        // }
+
         Clock { hours, minutes }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        let mut new_minutes = self.minutes + minutes;
+        let mut new_hours = self.hours;
+
+        match new_minutes >= 60 {
+            true => {
+                while new_minutes >= 60 {
+                    new_minutes -= 60;
+                    new_hours += 1;
+
+                }
+                Clock::new(new_hours, new_minutes)
+            },
+            false => {
+                while new_minutes < 0 {
+                    new_minutes += 60;
+                    new_hours -= 1;
+                }
+                Clock::new(new_hours, new_minutes)
+            }
+        }
     }
 }
 
