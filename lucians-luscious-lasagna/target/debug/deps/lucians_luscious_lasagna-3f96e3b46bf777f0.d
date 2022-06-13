9a56490736685154938291ec0217eb7cfc4728b5
João Junqueira <dev.junqueira@gmail.com>
2022-06-13 03:22:31 -0300

    Delete lucians-luscious-lasagna/target directory

--- lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-3f96e3b46bf777f0.d
+++
@@ -1,7 +0,0 @@
-/home/arch/exercism/rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-3f96e3b46bf777f0.rmeta: src/lib.rs
-
-/home/arch/exercism/rust/lucians-luscious-lasagna/target/debug/deps/liblucians_luscious_lasagna-3f96e3b46bf777f0.rlib: src/lib.rs
-
-/home/arch/exercism/rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-3f96e3b46bf777f0.d: src/lib.rs
-
-src/lib.rs:
--- lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-f3b1ca192471a5be.d
+++
@@ -1,5 +0,0 @@
-/home/arch/exercism/rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-f3b1ca192471a5be: src/lib.rs
-
-/home/arch/exercism/rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-f3b1ca192471a5be.d: src/lib.rs
-
-src/lib.rs:
