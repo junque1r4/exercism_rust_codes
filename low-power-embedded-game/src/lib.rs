9740d4a48232dff15c01f8c8d8b81c618f3b7924
junque1r4 <dev.junqueira@gmail.com>
2022-08-02 22:03:18 -0300

    task 2 done

--- low-power-embedded-game/src/lib.rs
+++ low-power-embedded-game/src/lib.rs
@@ -2,22 +2,20 @@
 // to enable stricter warnings.
 #![allow(unused)]
 
-pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
-    let division: i16 = dividend / divisor;
-    let sign: i16 = dividend % divisor;
+use std::ops::{Rem, Add};
 
+pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
     match dividend > divisor {
-        true => (division, sign),
-        false => (division, sign),
+        true => (dividend/divisor, dividend % divisor),
+        false => (dividend/divisor, dividend % divisor),
         _ => panic!("error")
     }
 }
 
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    unimplemented!("implement `fn evens`");
-    // TODO: remove this; it's only necessary to allow this function to compile
-    // before the student has done any work.
-    std::iter::empty()
+    iter.enumerate()
+    .filter(|(x, _)| x % 2 == 0)
+    .map(|(_, y)| y)
 }
 
 pub struct Position(pub i16, pub i16);
--- low-power-embedded-game/src/main.rs
+++
@@ -1,9 +0,0 @@
-fn main(){
-    let number_1: i32 = 10;
-    let number_2: i32 = 3;
-
-    println!("Divided: {}", number_1 / number_2);
-    println!("Sign: {}", number_1 % number_2);
-
-
-}
